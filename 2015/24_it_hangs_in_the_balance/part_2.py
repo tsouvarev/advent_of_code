373bc51907a8dfc839d2c0e42ec3ea78d0e77288
Ivan Tsuvarev <tsouvarev@mail.ru>
2025-10-04 10:47:35 +0300

    Day 24 of 2015

---
+++ 2015/24_it_hangs_in_the_balance/part_2.py
@@ -0,0 +1,48 @@
+"""
+That's weird... the sleigh still isn't balancing.
+
+"Ho ho ho", Santa muses to himself. "I forgot the trunk".
+
+Balance the sleigh again, but this time,
+separate the packages into four groups instead of three.
+The other constraints still apply.
+
+Given the example packages above, this would be some of the new unique first groups,
+their quantum entanglements, and one way to divide the remaining packages:
+
+11 4    (QE=44); 10 5;   9 3 2 1; 8 7
+10 5    (QE=50); 11 4;   9 3 2 1; 8 7
+9 5 1   (QE=45); 11 4;   10 3 2;  8 7
+9 4 2   (QE=72); 11 3 1; 10 5;    8 7
+9 3 2 1 (QE=54); 11 4;   10 5;    8 7
+8 7     (QE=56); 11 4;   10 5;    9 3 2 1
+
+Of these, there are three arrangements
+that put the minimum (two) number of packages in the first group: 11 4, 10 5, and 8 7.
+Of these, 11 4 has the lowest quantum entanglement, and so it is selected.
+
+Now, what is the quantum entanglement of the first group of packages
+in the ideal configuration?
+"""
+
+from functools import reduce
+from itertools import combinations
+from operator import mul
+
+
+def find_first_group(weights: list[int]) -> list[int]:
+    group_size = sum(weights) // 4
+    for i in range(len(weights)):
+        groups = [c for c in combinations(weights, i) if sum(c) == group_size]
+        if groups:
+            return min(reduce(mul, g) for g in groups)
+    return None
+
+
+assert find_first_group([1, 1, 1, 1]) == 1
+assert find_first_group([1, 2, 3, 4, 5, 7, 8, 9, 10, 11]) == 44
+
+
+with open("2015/24_it_hangs_in_the_balance/input.txt") as f:
+    weights = [int(line.strip()) for line in f]
+    print(find_first_group(weights))
