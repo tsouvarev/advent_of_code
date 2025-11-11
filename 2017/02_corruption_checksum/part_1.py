a76e500464e1af8aae5ef24ac5b2fd5e6dc9539e
Ivan Tsuvarev <tsouvarev@mail.ru>
2025-11-11 21:19:03 +0300

    Day 2 of 2017

---
+++ 2017/02_corruption_checksum/part_1.py
@@ -0,0 +1,50 @@
+"""
+As you walk through the door, a glowing humanoid shape yells in your direction.
+"You there! Your state appears to be idle.
+Come help us repair the corruption in this spreadsheet -
+if we take another millisecond, we'll have to display an hourglass cursor!"
+
+The spreadsheet consists of rows of apparently-random numbers.
+To make sure the recovery process is on the right track,
+they need you to calculate the spreadsheet's checksum.
+For each row, determine the difference between the largest value and the smallest value;
+the checksum is the sum of all of these differences.
+
+For example, given the following spreadsheet:
+
+5 1 9 5
+7 5 3
+2 4 6 8
+
+- The first row's largest and smallest values are 9 and 1, and their difference is 8.
+- The second row's largest and smallest values are 7 and 3, and their difference is 4.
+- The third row's difference is 6.
+
+In this example, the spreadsheet's checksum would be 8 + 4 + 6 = 18.
+
+What is the checksum for the spreadsheet in your puzzle input?
+"""
+
+from parse import parse
+
+
+def run_checksum(sheet: list[list[int]]) -> int:
+    checksum = 0
+
+    for row in sheet:
+        min_val, max_val = 1_000_000, 0
+        for col in row:
+            max_val = max(max_val, col)
+            min_val = min(min_val, col)
+
+        checksum += max_val - min_val
+
+    return checksum
+
+
+assert run_checksum([[5, 1, 9, 5], [7, 5, 3], [2, 4, 6, 8]]) == 18
+
+
+with open("2017/02_corruption_checksum/input.txt") as f:
+    sheet = [parse("{:\t<d}" * 16, line.strip()).fixed for line in f]
+    print(run_checksum(sheet))
